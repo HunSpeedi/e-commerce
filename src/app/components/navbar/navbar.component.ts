2601d15df6aca438288bc97ba435b9f071514b8c
Lóránd Véger <lorand.veger@movilitas.com>
2025-05-05 18:14:08 +0200

    main): Navbar component to use onPush and destroy subscription properly

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,26 +1,32 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { filter } from 'rxjs';
 
 import { CartService } from '../../services/cart.service';
-import { filter } from 'rxjs';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterLink]
 })
 export class NavbarComponent {
   public cartService = inject(CartService);
   public router = inject(Router);
-  public isCartPage = true;
+  public isCartPage = signal<boolean>(true)
 
   productsAmountInCart = this.cartService.productsAmountInCart;
 
   constructor() {
     this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        takeUntilDestroyed(),
+      )
       .subscribe((event: NavigationEnd) => {
-        this.isCartPage = event.url.startsWith('/cart');
+        this.isCartPage.set(event.url.startsWith('/cart'));
       });
   }
 }
